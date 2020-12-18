248de549e1c49a321aaf41509765412daa24e54f
sonali30 <rawatsonali3003@gmail.com>
2020-12-18 09:50:00 -0700

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes ,RouterModule} from '@angular/router';
+import {NgModule } from '@angular/core';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { FormValidationComponent } from './form-validation/form-validation.component';
+import { UserDetailComponent } from './user-detail/user-detail.component';
 
-const routes: Routes = [];
+const routes:Routes = [
+    {path: '',redirectTo :'/userdetail', pathMatch:'full'},
+    {path:' userdetail' ,component:UserDetailComponent},
+    {path:'formvalidation' ,component:FormValidationComponent},
+    {path:'**' ,component:PageNotFoundComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports :[RouterModule.forRoot(routes)],
+    exports :[RouterModule]
 })
+
 export class AppRoutingModule { }
+export const routingComponent = [UserDetailComponent,FormValidationComponent,PageNotFoundComponent]
+  
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule,routingComponent} from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule,ReactiveFormsModule } from '@angular/forms';
-import { UserFormComponent} from  './user-form/user-form.component';
-import { UserDetailComponent } from './user-detail/user-detail.component';
-import { FormValidationComponent } from './form-validation/form-validation.component';
+import { UserFormComponent } from './user-form/user-form.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     UserFormComponent,
-    UserDetailComponent,
-    FormValidationComponent
+    routingComponent
+  
   ],
   imports: [
     BrowserModule,

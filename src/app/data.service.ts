96b09916535fbc2ff7b4a8c6f4c509de00c1b05e
sonali30 <rawatsonali3003@gmail.com>
2020-12-24 10:01:19 -0700

    services Changes

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
- import { HttpClient, HttpErrorResponse} from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { IEmployee } from './employee';
 import { Observable } from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
@@ -10,31 +10,28 @@ import { EmployeesComponent } from './employees/employees.component';
   providedIn: 'root'
 })
 export class DataService {
-//  constructor() {}
-//   getEmployees(){ 
-//     return [
-//     {"id":1,"name":"Shona","age":20},
-//     {"id":2,"name":"Shiva","age":24},
-//     {"id":3,"name":"Jyoti","age":23}
-// ];
-//   }}
-    private _url:string="/assets/example/emloyees.json";
+  //  constructor() {}
+  //   getEmployees(){ 
+  //     return [
+  //     {"id":1,"name":"Shona","age":20},
+  //     {"id":2,"name":"Shiva","age":24},
+  //     {"id":3,"name":"Jyoti","age":23}
+  // ];
+  //   }}
+  private _url: string = "/assets/example/emloyees.json";
 
-    
-    
-    constructor(private http:HttpClient) 
-    {
-             console.log(1)
-     }
-     getEmployees():Observable<IEmployee[]>
-     {
-      console.log(2)
+
+
+  constructor(private http: HttpClient) {
+    console.log(1)
+  }
+  getEmployees(): Observable<IEmployee[]> {
+    console.log(2)
 
     return this.http.get<IEmployee[]>(this._url)
-    console.log(3)
 
-    }
-   };
-     
+  }
+};
 
- 
+
+

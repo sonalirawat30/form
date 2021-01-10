d40a604903b9a921c145bb15bfd82f8d4750bb57
sonali30 <rawatsonali3003@gmail.com>
2021-01-10 11:15:32 -0700

    array methods

--- src/app/serve/serve.component.ts
+++ src/app/serve/serve.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ServeService } from '../serve.service';
-import { filter,map,concatMap} from 'rxjs/operators';
-import { IServe } from  '../serve';
+import { filter, map, concatMap } from 'rxjs/operators';
+import { IServe } from '../serve';
 
 @Component({
   selector: 'app-serve',
@@ -13,31 +13,39 @@ export class ServeComponent implements OnInit {
 
   public serveList;
 
-  constructor( private serveService: ServeService) {}
+  constructor(private serveService: ServeService) { }
 
   ngOnInit(): void {
-    
-    this.serveService.getData().subscribe((response: any)=>{
+
+    this.serveService.getData().subscribe((response: any) => {
       this.serveList = response;
       console.log(this.serveList)
-     
-        this.serveList = this.serveList.filter(data=> data["username"].length>6)
-         console.log(this.serveList)
-       
-          this.serveList = this.serveList.map(data=>"Name:"+ data.name)
-          console.log(this.serveList)
 
-          const serve = this.serveList.pop();
-         console.log(serve)
+      const filter = this.serveList.filter(data => data["username"].length > 6)
+      console.log(filter)
 
-         const array = this.serveList.splice(0,1);
-         console.log(array)
+      const map = this.serveList.map(data => "Name:" + data.name)
+      console.log(map)
 
-         const s = this.serveList.shift();
-         console.log(s)
 
-         const r = this.serveList.unshift("xyz");
-         console.log(r)
-        
+      const serve = this.serveList.pop();
+      console.log(serve)
+
+      const array = this.serveList.splice(0, 1);
+      console.log(array)
+
+      const s = this.serveList.shift();
+      console.log(s)
+
+      const r = this.serveList.unshift("xyz");
+      console.log(r)
+
+
+      const z = this.serveList.push("abc");
+      console.log(z)
+
+      this.serveList = this.serveList
+        .forEach((element => console.log(element)));
     })
-}}
+  }
+}
